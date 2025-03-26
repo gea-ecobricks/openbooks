f0cd3df70914aee2feff587593e9efca668d1205
GEA Admin <64298789+gea-ecobricks@users.noreply.github.com>
2025-03-26 22:21:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html']))
-    message = """<!DOCTYPE html><html><head><title>OpenBooks Accounting</title></head><body><div style="font-family: Arial, sans-serif; text-align:center; padding: 20px;"><h1>Welcome to the Global Ecobrick Alliance OpenBooks accounting app</h1><h2>Beta Version</h2><p This is a beta app. The live version remains public at <a href="https://ecobricks.org/en/openbooks" target="_blank">ecobricks.org/openbooks</a></p><br><a href="https://github.com/gea-ecobricks/openbooks/" style="margin-right: 5px; padding: 10px 20px; font-size: 18px; background-color: #444; color: white; text-decoration: none; padding: 5px; border: none; cursor: pointer;">View GitHub Repository</a><br><a href="https://ecobricks.org/en/openbooks/" style="margin-right: 5px; padding: 10px 20px; font-size: 18px; background-color: #444; color: white; text-decoration: none; padding: 5px; border: none; cursor: pointer;">View current OpenBooks</a></div></body></html>"""
-    return [message.encode()]
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It really gotta gotta work!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
